1b908f54f735bd4804fb3c3355ec86aa4aa4b478
Richard Harrison <r.harr.k@gmail.com>
2024-08-19 12:18:24 +0100

    wraps endpoint return in a try catch block, to raise a 409 HTTP exception if username already exists

--- api/auth.py
+++ api/auth.py
@@ -1,14 +1,18 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 from database.database_utils.get_db import get_db
 
 from starlette import status
+from datetime import timedelta, datetime, timezone
 
 from api.models.user_models import User
 from api.schemas.user_schemas import CreateUserRequest
+from api.schemas.authentication_schemas import Token
 
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import jwt
 import bcrypt
 
 router = APIRouter(
@@ -24,6 +28,7 @@ oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = User(
@@ -31,9 +36,14 @@ def create_user(db: db_dependency, create_user_request: CreateUserRequest):
         hashed_password=hash_password(create_user_request.password)
     )
 
-    db.add(create_user_model)
-    db.commit()
-    db.refresh(create_user_model)
+    try:
+        db.add(create_user_model)
+        db.commit()
+        db.refresh(create_user_model)
+    except IntegrityError as e:
+        db.rollback()
+        raise HTTPException(status_code=409, detail="Username already exists.")
+
 
 def hash_password(password):
     pwd_bytes = password.encode('utf-8')
@@ -43,4 +53,4 @@ def hash_password(password):
 
 def verify_password(plain_password, hashed_password):
     password_byte_enc = plain_password.encode('utf-8')
-    return bcrypt.checkpw(password = password_byte_enc , hashed_password = hashed_password)
+    return bcrypt.checkpw(password=password_byte_enc, hashed_password=hashed_password)
