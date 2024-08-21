90dca93c1d648240a9ea5add810217b7c56cdee0
Richard Harrison <r.harr.k@gmail.com>
2024-08-21 14:18:06 +0100

    adds campsite/id/reviews endpoints to campsites route

--- api/routes/campsites.py
+++ api/routes/campsites.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from api.schemas.campsite_schemas import CampsiteDetailed, CampsiteCreateRequest, Campsite
 from typing import Annotated
 
 from database.database_utils.get_db import get_db
+
+from api.schemas.campsite_schemas import CampsiteDetailed, CampsiteCreateRequest, Campsite
+from api.schemas.review_schemas import ReviewCreateRequest, Review, ReviewUpdateRequest
+
 from api.crud.campsite_crud import create_campsite, read_campsites, read_campsite_by_id
+from api.crud.reviews_crud import create_review_by_campsite_id, read_reviews_by_campsite_id, update_review_by_review_id, remove_review_by_review_id
 
 from api.utils.security.authentication_utils import get_current_user
 
@@ -15,6 +19,8 @@ router = APIRouter(
 
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Depends(get_current_user)
+
+# router.include_router(reviews_route.router)
 
 @router.post("/", status_code=201, response_model=CampsiteDetailed)
 def post_campsite(request: CampsiteCreateRequest, db: db_dependency, user=user_dependency):
@@ -28,3 +34,17 @@ def get_campsites(skip: int = 0, limit: int = 250, db: Session = Depends(get_db)
 @router.get("/{campsite_id}", response_model=CampsiteDetailed)
 def get_campsite_by_campsite_id(campsite_id, db: Session = Depends(get_db), user=user_dependency):
     return read_campsite_by_id(db, campsite_id)
+
+@router.post("/{campsite_id}/reviews", status_code=201, response_model=Review)
+def post_review_by_campsite_id(campsite_id, request: ReviewCreateRequest, db: Session = Depends(get_db), user=user_dependency):
+    return create_review_by_campsite_id(db=db, campsite_id=campsite_id, request=request)
+
+
+@router.get("/{campsite_id}/reviews", response_model=list[Review])
+def get_reviews_by_campsite_id(campsite_id, db: Session = Depends(get_db), user=user_dependency):
+    return read_reviews_by_campsite_id(db, campsite_id)
+
+
+@router.patch("/{campsite_id}/reviews/{review_id}", status_code=200, response_model=Review)
+def patch_review_by_review_id(campsite_id, review_id, request: ReviewUpdateRequest, db: Session = Depends(get_db), user=user_dependency):
+    return update_review_by_review_id(db=db, request=request, campsite_id=campsite_id, review_id=review_id)
