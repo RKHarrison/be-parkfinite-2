7b681da0c587bbb4e16bc4f92fa3ec5d4133cc4a
Richard Harrison <r.harr.k@gmail.com>
2024-08-21 14:54:40 +0100

    injects user_dependency into whole review route

--- api/routes/reviews.py
+++ api/routes/reviews.py
@@ -5,15 +5,16 @@ from database.database_utils.get_db import get_db
 from api.utils.security.authentication_utils import get_current_user
 from api.crud.reviews_crud import remove_review_by_review_id
 
-
-router = APIRouter(
-    prefix='/reviews',
-    tags=['reviews']
-)
-
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Depends(get_current_user)
 
+router = APIRouter(
+    prefix='/reviews',
+    tags=['reviews'],
+    dependencies=[user_dependency]
+)
+
+
 @router.delete("/{review_id}", status_code=204)
-def delete_review_by_review_id(review_id, db: Session = Depends(get_db), user=user_dependency):
+def delete_review_by_review_id(review_id, db: Session = Depends(get_db)):
     return remove_review_by_review_id(db=db, review_id=review_id)
